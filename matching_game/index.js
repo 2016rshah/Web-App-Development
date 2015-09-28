38a9e638dae48e77b5e869a303643c2472b94918
Rushi Shah <2016rshah@gmail.com>
2015-09-28 14:20:52 -0400

    Basic functionality

--- matching_game/index.js
+++ matching_game/index.js
@@ -1,5 +1,7 @@
-var abbrToState = {"al":"alabama","ak":"alaska","az":"arizona","ar":"arkansas","ca":"california","co":"colorado","ct":"connecticut","de":"delaware","fl":"florida","ga":"georgia","hi":"hawaii","id":"idaho","il":"illinois","in":"indiana","ia":"iowa","ks":"kansas","ky":"kentucky","la":"louisiana","me":"maine","md":"maryland","ma":"massachusetts","mi":"michigan","mn":"minnesota","ms":"mississippi","mo":"missouri","mt":"montana","ne":"nebraska","nv":"nevada","nh":"new hampshire","nj":"new jersey","nm":"new mexico","ny":"new york","nc":"north carolina","nd":"north dakota","oh":"ohio","ok":"oklahoma","or":"oregon","pa":"pennsylvania","ri":"rhode island","sc":"south carolina","sd":"south dakota","tn":"tennessee","tx":"texas","ut":"utah","vt":"vermont","va":"virginia","wa":"washington","wv":"west virginia","wi":"wisconsin","wy":"wyoming"}
 $(document).ready(function() {
+	var abbrToState = {"al":"alabama","ak":"alaska","az":"arizona","ar":"arkansas","ca":"california","co":"colorado","ct":"connecticut","de":"delaware","fl":"florida","ga":"georgia","hi":"hawaii","id":"idaho","il":"illinois","in":"indiana","ia":"iowa","ks":"kansas","ky":"kentucky","la":"louisiana","me":"maine","md":"maryland","ma":"massachusetts","mi":"michigan","mn":"minnesota","ms":"mississippi","mo":"missouri","mt":"montana","ne":"nebraska","nv":"nevada","nh":"new hampshire","nj":"new jersey","nm":"new mexico","ny":"new york","nc":"north carolina","nd":"north dakota","oh":"ohio","ok":"oklahoma","or":"oregon","pa":"pennsylvania","ri":"rhode island","sc":"south carolina","sd":"south dakota","tn":"tennessee","tx":"texas","ut":"utah","vt":"vermont","va":"virginia","wa":"washington","wv":"west virginia","wi":"wisconsin","wy":"wyoming"}
+	var found_states = []
+	var status = document.getElementById("status")
 	$('#map').usmap({
 		stateHoverAnimation: 300,
 		showLabels:false,
@@ -10,7 +12,17 @@ $(document).ready(function() {
 
 			var guess = prompt("What is the name of the highlighted state?", "name or abbreviation").toLowerCase()
 			if(guess == name || guess == abbrToState[name]){
-				addState(abbrToState[name])
+				if(found_states.indexOf(abbrToState[name]) > -1){
+					status.style.backgroundColor = "yellow"
+					status.innerText="You've already found that state"
+				}
+				else{
+					addState(abbrToState[name])
+				}
+			}
+			else{
+				status.innerText = "That is incorrect"
+				status.style.backgroundColor = "red"
 			}
 
 			// $('#map').usmap({
@@ -22,8 +34,17 @@ $(document).ready(function() {
 
 		}
 	});
+	function addState(name){
+		found_states.push(name)
+		status.style.backgroundColor = "green"
+		status.innerText = "Nice!"
+		var f = document.getElementById("found")
+		if(f.innerText == ""){
+			f.innerText = name
+		}
+		else{
+			f.innerText = f.innerText + ", " + name 
+		}
+	}
 });
 
-function addState(name){
-	alert(name)
-}
