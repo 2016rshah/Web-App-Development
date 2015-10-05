5fa9f2422cb1d0f9aad3840b86c14ab3c9e804e0
Rushi Shah <2016rshah@gmail.com>
2015-10-05 16:42:26 -0400

    Made game work without mouse

--- matching_game/lazy.js
+++ matching_game/lazy.js
@@ -11,29 +11,16 @@ $(document).ready(function() {
 
 	var abbrToState = {"al":"alabama","ak":"alaska","az":"arizona","ar":"arkansas","ca":"california","co":"colorado","ct":"connecticut","de":"delaware","fl":"florida","ga":"georgia","hi":"hawaii","id":"idaho","il":"illinois","in":"indiana","ia":"iowa","ks":"kansas","ky":"kentucky","la":"louisiana","me":"maine","md":"maryland","ma":"massachusetts","mi":"michigan","mn":"minnesota","ms":"mississippi","mo":"missouri","mt":"montana","ne":"nebraska","nv":"nevada","nh":"new hampshire","nj":"new jersey","nm":"new mexico","ny":"new york","nc":"north carolina","nd":"north dakota","oh":"ohio","ok":"oklahoma","or":"oregon","pa":"pennsylvania","ri":"rhode island","sc":"south carolina","sd":"south dakota","tn":"tennessee","tx":"texas","ut":"utah","vt":"vermont","va":"virginia","wa":"washington","wv":"west virginia","wi":"wisconsin","wy":"wyoming"}
 	found_states = []
+	all_states = Object.keys(abbrToState)
+	curr_state = all_states[0]
+
+
 	var status = document.getElementById("status")
 	$('#map').usmap({
 		stateHoverAnimation: 300,
 		showLabels:false,
 		stateStyles: {fill: '#fff'},
-		stateHoverStyles : {fill: lightGreen},
-		click: function(event, data) {
-			var name = data.name.toLowerCase()
-			var guess = prompt("What is the name of the highlighted state?", "name or abbreviation").toLowerCase()
-			if(guess == name || guess == abbrToState[name]){
-				if(found_states.indexOf(abbrToState[name]) > -1){
-					status.style.backgroundColor = "yellow"
-					status.innerText="You've already found that state"
-				}
-				else{
-					addState(abbrToState[name], name)
-				}
-			}
-			else{
-				status.innerText = "That is incorrect"
-				status.style.backgroundColor = "red"
-			}
-		}
+		// stateHoverStyles : {fill: lightGreen}
 	});
 	String.prototype.capitalizeFirstLetter = function() {
 	    return this.charAt(0).toUpperCase() + this.slice(1);
@@ -70,5 +57,39 @@ $(document).ready(function() {
 		
 		redOrBlue == 0 ? redOrBlue = 1 : redOrBlue = 0 //ternary operator!
 	}
+	function shuffle(array) {
+	  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+	  // While there remain elements to shuffle...
+	  while (0 !== currentIndex) {
+
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+
+	    // And swap it with the current element.
+	    temporaryValue = array[currentIndex];
+	    array[currentIndex] = array[randomIndex];
+	    array[randomIndex] = temporaryValue;
+	  }
+
+	  return array;
+	}
+
+
+	$("#promptInput").on("input", function(){
+		var guess = $("#promptInput").val().toLowerCase()
+		console.log(guess)
+		if(guess == curr_state || guess == abbrToState[curr_state]){
+			addState(abbrToState[curr_state], curr_state)
+			all_states.shift()
+			curr_state = all_states[0]
+			$("#"+curr_state.toUpperCase()).css('fill', "blue")
+			$("#promptInput").val("")
+		}
+	})
+
+
+	$("#"+curr_state.toUpperCase()).css('fill', "blue")
 });
 
