6a47540369c4ebcef6eeed94ef732ffe940d2d81
Rushi Shah <2016rshah@gmail.com>
2015-11-04 18:14:28 -0500

    Cleared up code

--- trees/index.js
+++ trees/index.js
@@ -1,16 +1,13 @@
 currElem = null
-
 document.body.addEventListener('click', function(e){
-	if(currElem)
-		borderElement(currElem, "")
+	if(currElem) borderElement(currElem, "")
 	currElem = e.target
 	borderElement(currElem, "1px solid red")
 })
-
-function highlightElement(elem, color){
-	elem.style.backgroundColor = color
-}
-
 function borderElement(elem, border){
 	elem.style.border = border
 }
+
+// function highlightElement(elem, color){
+// 	elem.style.backgroundColor = color
+// }
