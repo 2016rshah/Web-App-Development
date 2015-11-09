c8d7590ea9195edc9e7bce3a3cfd6a67e6b44248
Rushi Shah <2016rshah@gmail.com>
2015-11-09 15:25:18 -0500

    Renamed and finished part 1

--- canvas/index.js
+++ canvas/index.js
@@ -1,6 +1,9 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 
+var dots = []
+
+
 //http://stackoverflow.com/a/5932203/3861396
 function relMouseCoords(event){
     var totalOffsetX = 0;
@@ -24,13 +27,32 @@ HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
 
 c.onclick = function(e) {
 
-	//clear the canvas
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	ctx.strokeStyle = "#00FF00";
+	coords = canvas.relMouseCoords(e);
+	canvasX = coords.x;
+	canvasY = coords.y;
+	console.log(canvasX, canvasY)
+
+
+	ctx.fillStyle = "blue";
 	ctx.beginPath();
-	ctx.arc(canvasX,canvasY,40,0,2*Math.PI);
-	ctx.stroke();
+	ctx.arc(canvasX,canvasY,20,0,2*Math.PI);
+	ctx.fill();
+    dots.push({x:canvasX, y:canvasY, r:20, c:"blue"})
+}
+document.getElementById("clear").onclick = function(){
+
+    //clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    dots = []
 }
 
+function drawDots(arr){
+    for(var i = 0; i<arr.length; i++){
+        ctx.fillStyle = arr[i].c
+        ctx.beginPath();
+        ctx.arc(arr[i].x, arr[i].y, arr[i].r, 0,2*Math.PI)
+        ctx.fill()
+    }
+}
 
