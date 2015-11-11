f1005b00110bbc4084307069ef054342ec840a4b
Rushi Shah <2016rshah@gmail.com>
2015-11-11 13:13:28 -0500

    Continued lab

--- canvas/index.js
+++ canvas/index.js
@@ -3,13 +3,11 @@ var ctx = c.getContext("2d");
 
 var dots = []
 
-
 //http://stackoverflow.com/a/5932203/3861396
 function relMouseCoords(event){
     var totalOffsetX = 0;
     var totalOffsetY = 0;
-    var canvasX = 0;
-    var canvasY = 0;
+    var coords = {x:0, y:0}
     var currentElement = this;
 
     do{
@@ -18,33 +16,17 @@ function relMouseCoords(event){
     }
     while(currentElement = currentElement.offsetParent)
 
-    canvasX = event.pageX - totalOffsetX;
-    canvasY = event.pageY - totalOffsetY;
+    coords.x = event.pageX - totalOffsetX;
+    coords.y = event.pageY - totalOffsetY;
 
-    return {x:canvasX, y:canvasY}
+    return {x:coords.x, y:coords.y}
 }
 HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
 
-c.onclick = function(e) {
+document.getElementById("clear").onclick = clearC
 
-
-	coords = canvas.relMouseCoords(e);
-	canvasX = coords.x;
-	canvasY = coords.y;
-	console.log(canvasX, canvasY)
-
-
-	ctx.fillStyle = "blue";
-	ctx.beginPath();
-	ctx.arc(canvasX,canvasY,20,0,2*Math.PI);
-	ctx.fill();
-    dots.push({x:canvasX, y:canvasY, r:20, c:"blue"})
-}
-document.getElementById("clear").onclick = function(){
-
-    //clear the canvas
+function clearC(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    dots = []
 }
 
 function drawDots(arr){
@@ -54,5 +36,93 @@ function drawDots(arr){
         ctx.arc(arr[i].x, arr[i].y, arr[i].r, 0,2*Math.PI)
         ctx.fill()
     }
+}
+
+function drawTwoPointRect(l1, l2){
+    var ix = l1.x
+    var iy = l1.y
+    var dx = l2.x - l1.x
+    var dy = l2.y - l1.y
+    ctx.rect(ix, iy, dx, dy);
+    ctx.stroke();
+}
+
+function convertDots(startLoc, finalLoc){
+    var xi = startLoc.x
+    var xf = finalLoc.x
+    var yi = startLoc.y
+    var yf = finalLoc.y
+
+    for(var i = 0; i<dots.length; i++){
+        var xt = dots[i].x
+        var yt = dots[i].y
+
+        if((xt > xi && xt < xf) || (xt < xi && xt > xf)){ //xs pass
+            if((yt > yi && yt < yf) || (yt < yi && yt > yf)){ //ys pass
+                dots[i].c = "red"
+            }
+        }
+    }
+}
+
+function resetDots(){
+    for(var i = 0; i<dots.length; i++){
+        dots[i].c = "blue"
+    }
+}
+
+var startLoc = {}
+var currLoc = {}
+var finalLoc = {}
+
+var drawing = false;
+
+c.onmousedown = function(e){
+    var coords = canvas.relMouseCoords(e);
+    startLoc = {x:coords.x, y:coords.y}
+
+    drawing = true;
+}
+c.onmousemove = function(e){
+    if(drawing){
+        clearC()
+
+        var coords = canvas.relMouseCoords(e);
+        currLoc = {x:coords.x, y:coords.y}
+        drawTwoPointRect(startLoc, currLoc)
+        
+        drawDots(dots)
+    }
+}
+c.onmouseup = function(e){
+
+    drawing = false;
+
+    var coords = canvas.relMouseCoords(e);
+
+    finalLoc = {x:coords.x, y:coords.y}
+
+    if(finalLoc.x == startLoc.x && finalLoc.y == startLoc.y){
+        clearC()
+        dots.push({x:coords.x, y:coords.y, r:20, c:"blue"})
+        drawDots(dots)
+    }
+    else{
+
+        clearC()
+
+        console.log(e.ctrlKey, event.button == 2 )
+        if(!e.ctrlKey){
+            resetDots()
+        }
+
+
+        convertDots(startLoc, finalLoc)
+
+        drawDots(dots)
+    }
+    startLoc = {}
+    currLoc = {}
+    finalLoc = {}
 }
 
