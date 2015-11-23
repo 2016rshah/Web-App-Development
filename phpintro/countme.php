b7a3374ad4bb94fabd15442c56ed750301bd2ca6
Rushi Shah <2016rshah@gmail.com>
2015-11-23 13:46:04 -0500

    Pretty colors

--- phpintro/countme.php
+++ phpintro/countme.php
@@ -1,4 +1,17 @@
-
+<html>
+	<head>
+		<title>Visitor Count</title>
+		<style>
+			body{
+				text-align:center;
+				font-size:20pt;
+				color:#FFF;
+				background-color:#3d97ca;
+			}
+		</style>
+	</head>
+	<body>
+	<p>Visitor count:
 <?php
 	$views = 0;
 	$visitors_file = "visitors.txt";
@@ -13,3 +26,6 @@
 
 	file_put_contents($visitors_file, $views)
 ?>
+	</p>
+	</body>
+</html>
