9abf963c99822897367bf9cf05fe7533f7c427fd
Rushi Shah <2016rshah@gmail.com>
2015-12-02 13:59:21 -0500

    Got lab checked off

--- form_basics/index.php
+++ form_basics/index.php
@@ -23,10 +23,16 @@
 			<h3>Tell me about yourself</h3>
 			<form action="index.php" method="post">
 				<label for="name">Name</label>
-			    <div id="name"><input type="text" name="namef" placeholder="First"><input type="text" name="namem" size="2" placeholder="M"><input type="text" name="namel" placeholder="Last"></div>
+			    <div id="name"><input autofocus="autofocus" type="text" name="namef" placeholder="First"><input type="text" name="namem" size="2" placeholder="M"><input type="text" name="namel" placeholder="Last"></div>
 				<label for="phone">Phone Number</label><input id="phone" type="text" name="phone">
 			    <label for="birthday">Birthday</label><input id="birthday" type="text" name="birthday">
-				<label for="food">Favorite Food</label><input id="food" type="text" name="food">
+				<label for="food">Favorite Food</label>
+				<select id="food" name="food">
+					<option value="Mexican">Mexican Cuisine</option>
+					<option value="Thai">Thai Cuisine</option>
+					<option value="Indian">Indian Cuisine</option>
+					<option value="Italian">Italian Cuisine</option>
+				</select>
 				<br>
 			    <button class="button-primary input" type="submit" accessKey="s"><u>S</u>ubmit</button>
 			</form>
@@ -39,7 +45,7 @@
 						<p><b>Name:</b> <?php echo $_REQUEST["namef"], " ", $_REQUEST["namem"], " ", $_REQUEST["namel"]; ?></p>
 						<p><b>Phone:</b> <?php echo $_REQUEST["phone"]; ?></p>
 						<p><b>Birthdate:</b> <?php echo $_REQUEST["birthday"]; ?></p>
-						<p><b>Favorite Food:</b> <?php echo $_REQUEST["food"]; ?></p>
+						<p><b>Favorite Food:</b> <?php echo $_POST["food"]; ?></p>
 						<p><b>Time:</b> <?php echo date('Y-m-d H:i:s'); ?></p>
 					</div>
 				</div>
